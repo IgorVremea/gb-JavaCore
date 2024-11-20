2a0f2bc32986e73093bb972419feb405270b1189
Igor Vremea <igori.vremya@gmail.com>
2024-11-21 01:39:01 +0200

    Backup

--- src/main/java/FileManager.java
+++ src/main/java/FileManager.java
@@ -1,61 +1,52 @@
 import java.io.*;
-import java.util.ArrayList;
 
 public class FileManager {
-    String path;
+    private static final String BACKUP_PATH = "./backup";
+    private String path;
     public FileManager(String path){
         this.path = path;
     }
-
-    public void writeArrToFile(int[] arr){
-        try(FileWriter fWriter = new FileWriter(path)) {
-            fWriter.write(arrToStrWithSpace(arr));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public boolean isDir(){
+        File file = new File(path);
+        return file.isDirectory();
     }
-    private String arrToStrWithSpace(int[] arr){
-        StringBuilder stringBuilder = new StringBuilder();
-        for(int i : arr){
-            stringBuilder.append(i).append(" ");
-        }
-        stringBuilder.deleteCharAt(stringBuilder.length()-1);
-        return stringBuilder.toString();
+    public String[] getDirFileList(){
+        File file = new File(path);
+        return file.list();
     }
-    public int[] readArrFromFile(){
-        String temp;
-        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(path))){
-            String[] stringArr = bufferedReader.readLine().split(" ");
-            int[] tempArr = new int[stringArr.length];
-            for(int i = 0; i < tempArr.length; i++){
-                tempArr[i] = Integer.parseInt(stringArr[i]);
+    public void doBackup() {
+        File file = new File(BACKUP_PATH);
+        file.mkdir();
+        for(String path : getDirFileList()){
+            if(path.indexOf('.')!=-1){
+                try(FileWriter fileWriter = new FileWriter(BACKUP_PATH + "/" + path); FileReader fileReader = new FileReader(this.path + "/" + path)) {
+                    int intIn;
+                    while( (intIn = fileReader.read()) != -1){
+                        fileWriter.append((char)intIn);
+                    }
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                    e.printStackTrace();
+                }
             }
-            return tempArr;
-        } catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-        return null;
     }
-    public void changeCharToSpace(char c){
-        try(FileReader fileReader = new FileReader(path); FileWriter fileWriter = new FileWriter(insertWordInPath(path, "Changed"))) {
-            int tempChar;
-            while( (tempChar = fileReader.read())  != -1){
-                fileWriter.append((char) tempChar == c ? ' ' : (char) tempChar);
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println("File wasn't find");
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void deleteBackup(){
+        deleteDirRecurs(BACKUP_PATH);
     }
 
-    private String insertWordInPath(String where, String what) {
-        int indexOfDot = where.indexOf('.');
-        StringBuilder stringBuilder = new StringBuilder(where);
-        stringBuilder.insert(indexOfDot, what);
-        return stringBuilder.toString();
+    public void deleteDirRecurs(String backupPath) {
+        File file = new File(backupPath);
+        File[] children = file.listFiles();
+        if ((children == null) || (file.isFile())){
+            file.delete();
+        } else {
+            for(File child : children){
+                deleteDirRecurs(child.getPath());
+            }
+        }
+        file.delete();
     }
+
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,16 +3,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
+    public static final String PATH = "src/main/resources";
     public static void main(String[] args) {
-
-//        int[] arr = {-1, 2, 20, 34, -123, 888, 0};
-        int[] arr;
-
-        FileManager fileManager = new FileManager("src/main/resources/fileChangedtxt");
-//        fileManager.writeArrToFile(arr);
-//        arr = fileManager.readArrFromFile();
-//        System.out.println(Arrays.toString(arr));
-
-        fileManager.changeCharToSpace('o');
+        FileManager fileManager = new FileManager(PATH);
+        System.out.println(Arrays.toString(fileManager.getDirFileList()));
+//        fileManager.doBackup();
+//        fileManager.deleteBackup();
     }
 }
